40224b058c6bcb99db29d9db5ef2aed9a4d5442d
mark ogujiuba <markogujiuba@gmail.com>
2023-11-30 21:26:54 +0100

    adjusted take header in catgory

--- src/app/(admin)/categories/categoryList.jsx
+++ src/app/(admin)/categories/categoryList.jsx
@@ -23,7 +23,7 @@ const CategoryList = async () => {
                   scope="col"
                   className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Name
+                  No.
                 </th>
                 <th
                   scope="col"
@@ -35,13 +35,19 @@ const CategoryList = async () => {
                   scope="col"
                   className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
+                  Content
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
                   Status
                 </th>
                 <th
                   scope="col"
                   className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Role
+                  Featured Image
                 </th>
                 <th scope="col" className="relative px-6 py-3">
                   <span className="sr-only">Edit</span>
