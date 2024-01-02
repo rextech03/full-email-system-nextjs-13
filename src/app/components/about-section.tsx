eed755bad4826bb480895695d0c376732f72b8ac
mark ogujiuba <markogujiuba@gmail.com>
2024-01-02 21:57:14 +0100

    news

--- src/app/components/about-section.tsx
+++ src/app/components/about-section.tsx
@@ -5,26 +5,28 @@ import React from 'react'
 export default function AboutSection() {
   return (
     <div className='container py-4'>
-        <div className="bg-gray-200 rounded-lg">
-
         
-        <div className="flex ">
-            <div className="w-1/3 ">
-                <Image src={'/images/phoenixlogo.png'} alt='Phoenix logo' width={94} height={124} className='w-1/3 h-40' />
+
+        <h3 className='pl-60 ml-60 pb-4 text-5xl font-bold'>About Us</h3>
+        <div className="flex bg-gray-200 rounded-lg relative ">
+        
+            <div className="w-1/3 absolute bottom-2">
+                <Image src={'/images/phoenixlogo.png'} alt='Phoenix logo' width={224} height={224} className='w-96 h-96' />
             </div>
-            <div className='w-2/3'>
-            <h3>About Us</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            <div className='ml-60 pl-56 py-12'>
+            
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    <br /> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
                 Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Id donec ultrices.</p>
                     
-                <div className="py-4">
+                <div className="py-12">
                     <Link href="/" className="py-4 px-12 rounded-full bg-blue-800 text-white">
                         Learn More
                 </Link>
                 </div>
             </div>
         </div>
-        </div>
+       
     </div>
   )
 }
